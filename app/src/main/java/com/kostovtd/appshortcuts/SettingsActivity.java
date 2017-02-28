3afdce0d58a180e4de1fd8ebbacc459df467ea82
Todor Kostov <kostovtd@gmail.com>
2017-02-28 23:18:46 +0200

    Static App shortcuts included. String resources included.

--- app/src/main/java/com/kostovtd/appshortcuts/SettingsActivity.java
+++ app/src/main/java/com/kostovtd/appshortcuts/SettingsActivity.java
@@ -1,17 +1,17 @@
 package com.kostovtd.appshortcuts;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+/**
+ * Created by kostovtd on 28.02.17.
+ */
 
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
     }
-
-
 }
